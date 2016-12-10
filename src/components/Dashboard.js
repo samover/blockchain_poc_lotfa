613c1d442e62330367e7e7590abc00e8a5a0c571
Brent Van Geertruy <brentvangeertruy@outlook.com>
2016-12-10 16:52:58 +0100

    add transactions table overview

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,53 +3,63 @@ import React, { Component } from 'react';
 class Dashboard extends Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = { transactions: [] }
     }
 
     componentDidMount() {
+        this.getTransactions();
+    }
 
+    getTransactions() {
+        // TODO: real function
+        this.setState({
+            transactions: [
+                {
+                    block: 'jfsdfkjhsdfhsdkjf212#edesf',
+                    description: 'A shipment was received on 15/10/2016 at 03:20:21'
+                },
+                {
+                    block: 'arze2135#21edzae#edesf',
+                    description: 'A shipment was sent on 15/10/2016 at 03:20:30'
+                },
+                {
+                    block: '823jklf#lkfjsdlkgjdfgkljdf',
+                    description: 'A shipment was received on 15/10/2016 at 03:20:43'
+                }
+            ]
+        });
     }
 
     render() {
+        var transactions = this.state.transactions.map((transaction, i) => {
+            return (
+                <tr key={i}>
+                    <td>{transaction.block}</td>
+                    <td>{transaction.description}</td>
+                </tr>
+            );
+        });
+
         return (
             <div>
                 <div id="page-title">
-                    <h2>Dashboard</h2>
-                    <p>The most complete user interface framework that can be used to create stunning admin dashboards
-                        and presentation websites.</p>
+                    <h2>Overview</h2>
+                    <p>All your transactions are available below. These transactions are saved into a private
+                        blockhain. </p>
                 </div>
 
-                <div className="row">
-                    <div className="col-md-4">
-                        <div className="dashboard-box dashboard-box-chart bg-white content-box">
-                            <div className="content-wrapper">
-                                <div className="header">
-                                    $34,657
-                                    <span>Total Earnings<b> in last</b> ten <b>quarters</b></span>
-                                </div>
-                                <div className="bs-label bg-green">+18%</div>
-                                <div className="center-div sparkline-big-alt"></div>
-                                <div className="row list-grade">
-                                    <div className="col-md-2">January</div>
-                                    <div className="col-md-2">February</div>
-                                    <div className="col-md-2">March</div>
-                                    <div className="col-md-2">April</div>
-                                    <div className="col-md-2">May</div>
-                                    <div className="col-md-2">June</div>
-                                </div>
-                            </div>
-                            <div className="button-pane">
-                                <div className="size-md float-left">
-                                    <a href="#" title="">
-                                        Financial statistics
-                                    </a>
-                                </div>
-                                <a href="#" className="btn btn-info float-right tooltip-button" data-placement="top" title="" data-original-title="View details">
-                                    <i className="glyph-icon icon-plus"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                <div className="example-box-wrapper">
+                    <table className="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>Block</th>
+                            <th>Description</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {transactions}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         );
