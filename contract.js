8da54b52f9b318a00e9bf95dd43e2a8868625ce5
Pieter Moeyersons <pieter.moeyerson@icapps.com>
2016-12-10 18:51:39 +0100

    block watcher + contract listener

--- contract.js
+++ contract.js
@@ -22,25 +22,44 @@ if (!web3.isConnected()) {
 console.info('Connected...');
 
 function init() {
-    return fs.readFile(path.join(__dirname, 'contracts/myContract.sol'), 'utf-8', (err, result) => {
+    fs.readFile(path.join(__dirname, 'contracts/myContract.sol'), 'utf-8', (err, result) => {
         const contractSource = result.replace(/\n/g, '');
-        const contractCompiled = web3.eth.compile.solidity(contractSource)
+        const contractCompiled = web3.eth.compile.solidity(contractSource);
 
-        const myContract = web3.eth.contract(contractCompiled.info.abiDefinition);
 
-        const contractInstance = myContract.new(1000, {from:web3.eth.accounts[0]}, function(e, contract){
-            if(!e) {
+        const myContract = web3.eth.contract(contractCompiled.MyContract.info.abiDefinition);
 
-                if(!contract.address) {
+        const contractInstance = myContract.new({from: web3.eth.accounts[0]}, function (e, contract) {
+            console.log('error', e);
+            if (!e) {
+
+                if (!contract.address) {
                     console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
-
                 } else {
                     console.log("Contract mined! Address: " + contract.address);
                     console.log(contract);
+                    contract.transferOwner(web3.eth.accounts[1], function (err, newOwner) {
+                        console.log('new owner', err, newOwner);
+                        console.log(contractInstance.getOwner() === newOwner);
+                    });
                 }
-
             }
-        })
+        });
+
+
+        var filter = web3.eth.filter('latest');
+
+        filter.watch(function (error, result) {
+
+            var block = web3.eth.getBlock(result, true);
+            console.log(block.number);
+
+            for (var index = 0; index < block.transactions.length; index++) {
+                var t = block.transactions[index];
+                console.log('transaction found', t.hash);
+            }
+
+        });
     });
 }
 
