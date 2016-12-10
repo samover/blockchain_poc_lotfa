b2e9145405ca194ba4023bc14f545414437b981d
Maarten Anckaert <maarten.anckaert@icapps.com>
2016-12-10 18:01:38 +0100

    basic contract setup

--- src/services/Web3Service.js
+++ src/services/Web3Service.js
@@ -10,4 +10,11 @@ const getWeb3 = (web3) => {
   return web3 ? new Web3(web3.currentProvider) : new Web3(new Web3.providers.HttpProvider(backendPath));
 };
 
+const deployNewContract = (abiArray, address, web3Instance) => {
+  const web3 = getWeb3(web3Instance);
+  const { contract } = web3.eth;
+  const myContract = contract(abiArray);
+
+};
+
 export default getWeb3;
